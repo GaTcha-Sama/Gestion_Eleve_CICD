c7664040c6c7de5bf3a8af293f954fbca878b569
Jeremy Louet <jeremy.louet29@hotmail.fr>
2025-04-17 10:08:17 +0200

    Modify: naming class Eleve to class Etudiant

--- Eleve.java
+++
@@ -1,7 +0,0 @@
-public class Eleve {
-    private String nom;
-    private String prenom;
-    private int age;
-    private String classe;
-    
-}
---
+++ Etudiant.java
@@ -0,0 +1,7 @@
+public class Etudiant {
+    private String nom;
+    private String matricule;
+    private int dateNaissance;
+    private String notes;
+    
+}
