5b646dfe62db12bbcc72d27f62e5bbd0eb8b5624
Polodys <136794468+Polodys@users.noreply.github.com>
2025-04-18 08:57:59 +0200

    Classe Etudiant Getters

--- Etudiant.java
+++ Etudiant.java
@@ -1,9 +1,38 @@
-public class Etudiant {
-    private String nom;
-    private String adresse;
-    private int dateNaissance;
-    private List<double> notes;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-    
-    
+public class Etudiant {
+    private String matricule;
+    private String nom;
+    private LocalDate dateNaissance;
+    private List<Double> notes;
+
+    public Etudiant(String matricule, String nom, LocalDate dateNaissance) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.dateNaissance = dateNaissance;
+        this.notes = new ArrayList<>();
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public List<Double> getNotes() {
+        return notes;
+    }
+
+    public void ajouterNote(double note) {
+        this.notes.add(note);
+    }
+
 }
