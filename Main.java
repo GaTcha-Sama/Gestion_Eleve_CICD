f50dc44a81f867e2c5274869ba4a94d4443efae1
Jeremy Louet <jeremy.louet29@hotmail.fr>
2025-04-18 09:12:43 +0200

    Add: functions enregistrerEtudiant & afficher moyenne

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-
 public class Main {
     private static Etudiant[] etudiants;
     private static Scanner scanner = new Scanner(System.in);
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static void main(String[] args) {
 
         System.out.print("Entrez le nombre d'etudiants: ");
         int nbEtud = scanner.nextInt();
         etudiants = new Etudiant[nbEtud];
+
+        enregistrerEtudiant();
 
         while (true) {
             afficherMenu();
@@ -30,6 +33,9 @@ public class Main {
                     reinitialiserListe();
                     break;
                 case 6:
+                    afficherMoyenne();
+                    break;
+                case 7:
                     System.out.println("Au revoir!");
                     scanner.close();
                     return;
@@ -39,13 +45,29 @@ public class Main {
         }
     }
 
+    private static void enregistrerEtudiant() {
+        for (int i = 0; i < etudiants.length; i++) {
+            System.out.println("Enregistrement de l'etudiant " + (i + 1) + ":");
+            System.out.print("Nom: ");
+            String nom = scanner.next();
+            System.out.print("Matricule: ");
+            String matricule = scanner.next();
+            System.out.print("Date de naissance (dd/MM/yyyy): ");
+            String dateNaissance = scanner.next();
+            System.out.print("Notes: ");
+            String notes = scanner.next();
+        }
+    }
+
     private static void afficherMenu() {
+        System.out.println("\n ###### Menu: ######");
         System.out.println("1. Afficher par mérite");
         System.out.println("2. Afficher le premier étudiant");
         System.out.println("3. Ajouter le dernier étudiant");
         System.out.println("4. Afficher les notes");
         System.out.println("5. Réinitialiser la liste");
-        System.out.println("6. Quitter");
+        System.out.println("6. Afficher la moyenne");
+        System.out.println("7. Quitter");
         System.out.print("Entrez votre choix: ");
     }
 
@@ -63,6 +85,10 @@ public class Main {
 
     private static void afficherNotes() {
         System.out.println("Afficher les notes");
+    }
+
+    private static void afficherMoyenne() {
+        System.out.println("Afficher la moyenne");
     }
 
     private static void reinitialiserListe() {
